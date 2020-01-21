f0aef882722c122d05e665a9de01306b8cfc5610
JorgeOchoaDev <jl91ob@protonmail.com>
2020-01-20 19:10:34 -0600

    ver 1.00

---
+++ index.js
@@ -0,0 +1,11 @@
+/*
+* Tengo una aplicación donde la gente guarda sus tarjetas de crédito para compras con un click.
+* Por seguridad, quiero hacer una función que tome sus números de tarjeta de crédito y reemplace todos por un asterisco * excepto los últimos 4
+* Ej: si numeros = "371449635398431" entonces ocultar(numeros) me debe devolver "***********8431"
+*/
+
+var numeros = ""
+
+function ocultar () {
+
+}
